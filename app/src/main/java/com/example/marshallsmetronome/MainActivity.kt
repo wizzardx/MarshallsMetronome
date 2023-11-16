6f07a2f589a5fce92bce78c4f5d7f86d8c7eeabf
david <wizzardx@gmail.com>
2023-11-16 11:04:06 +0200

    Move Constants to a separate file

--- app/src/main/java/com/example/marshallsmetronome/MainActivity.kt
+++ app/src/main/java/com/example/marshallsmetronome/MainActivity.kt
@@ -168,41 +168,6 @@ enum class IntervalType {
 }
 
 /**
- * Constants for our app.
- */
-object Constants {
-    /**
-     * The first interval that occurs in a Cycle is "Work".
-     */
-    val FirstIntervalTypeInCycle = IntervalType.Work
-
-    /**
-     * The last interval that occurs in a Cycle is "Rest".
-     */
-    val LastIntervalTypeInCycle = IntervalType.Rest
-
-    /**
-     * There are 60 seconds in every minute.
-     */
-    const val SecondsPerMinute = 60
-
-    /**
-     * There are 1000 milliseconds in every second.
-     */
-    const val MillisecondsPerSecond = 1000
-
-    /**
-     * In our logic we delay for short periods of time and then perform timer-related logic.
-     */
-    const val SmallDelay = 100
-
-    /**
-     * For the sake of simplicity, we limit the user to inputting numbers between 1 and 100.
-     */
-    const val MaxUserInputNum = 100
-}
-
-/**
  * Manages sound playback for various events in the application.
  *
  * This class encapsulates all sound-related operations, providing methods to play specific sounds
